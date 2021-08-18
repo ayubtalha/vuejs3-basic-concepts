560dd9d2a2f9934958f1b262f55fef9271a67bd6
Talha-Ayub-github <mtalhaayub752@gmail.com>
2021-08-18 06:07:55 +0500

    VUE JS 3 - Youtube Ninja. Modal Project with all VUE JS 3 basic concepts

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,112 @@
+<!--USING TELEPORT - CHANGES IN INDEX.HTML-->
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <p>Welcome ...</p>
+  <teleport to=".modals" v-if="showModal">
+    <Modal theme="sale" @close="toggleModal">
+      <template v-slot:links-one>
+        <a href="#">Modal Slot 1</a>
+      </template>
+    </Modal>
+  </teleport>
+
+  <div v-if="showModalTwo">
+    <Modal @close="toggleModalTwo">
+      <template v-slot:links-two>
+        <a href="#">Modal Slot2</a>
+      </template>
+    </Modal>
+  </div>
+  <button @click="toggleModal">Show Modal</button>
+  <button @click="toggleModalTwo">Show Modal 2</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from "./components/Modal.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: "App",
+  components: { Modal },
+  data() {
+    return {
+      title: "My first vue App :) ",
+      showModal: false,
+      showModalTwo: false,
+    };
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+    toggleModalTwo() {
+      this.showModalTwo = !this.showModalTwo;
+    },
+  },
+};
+</script>
+
+<style>
+#app,
+.modals {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
+h1 {
+  background-color: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
+}
+</style>
+
+<!--CHALLENGE - CREATING ANOTHER MODAL
+<template>
+  <h1>{{ title }}</h1>
+  <p>Welcome ...</p>
+  <div v-if="showModal">
+    <Modal theme="sale" @close="toggleModal">
+      <template v-slot:links-one>
+        <a href="#">Modal Slot 1</a>
+      </template>
+    </Modal>
+  </div>
+
+  <div v-if="showModalTwo">
+    <Modal @close="toggleModalTwo">
+      <template v-slot:links-two>
+        <a href="#">Modal Slot2</a>
+      </template>
+    </Modal>
+  </div>
+  <button @click="toggleModal">Show Modal</button>
+  <button @click="toggleModalTwo">Show Modal 2</button>
+</template>
+
+<script>
+import Modal from "./components/Modal.vue";
+
+export default {
+  name: "App",
+  components: { Modal },
+  data() {
+    return {
+      title: "My first vue App :) ",
+      showModal: false,
+      showModalTwo: false,
+    };
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+    toggleModalTwo() {
+      this.showModalTwo = !this.showModalTwo;
+    },
+  },
+};
 </script>
 
 <style>
@@ -23,4 +118,282 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+h1 {
+  background-color: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
+}
 </style>
+-->
+
+<!--SLOTS
+<template>
+  <h1>{{ title }}</h1>
+  <p>Welcome ...</p>
+  <div v-if="showModal">
+    <Modal theme="sale" @close="toggleModal">
+      // Named slots
+      <template v-slot:links>
+        <a href="#">Sign up Now</a>
+      </template>
+      // Default Slots 
+      //  <h2>
+      //   Here we are Using <span><h1>SLOT</h1></span>
+      // </h2>
+      // <p>Used SLOT</p>
+    </Modal>
+  </div>
+  <button @click="toggleModal">Show Modal</button>
+</template>
+
+<script>
+import Modal from "./components/Modal.vue";
+
+export default {
+  name: "App",
+  components: { Modal },
+  data() {
+    return {
+      title: "My first vue App :) ",
+      header: "Please sign up here",
+      text: "It is all coming from App.vue which is the parent component",
+      showModal: false,
+    };
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1 {
+  background-color: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
+}
+</style>
+-->
+
+<!-- CLICK EVENTS MODIFIERS
+<template>
+  <h1>{{ title }}</h1>
+  <p>Welcome ...</p>
+  <div v-if="showModal">
+    <Modal :header="header" :text="text" theme="sale" @close="toggleModal" />
+  </div>
+  <button @click.alt="toggleModal">Show Modal (alt)</button>
+</template>
+
+<script>
+import Modal from "./components/Modal.vue";
+
+export default {
+  name: "App",
+  components: { Modal },
+  data() {
+    return {
+      title: "My first vue App :) ",
+      header: "Please sign up here",
+      text: "It is all coming from App.vue which is the parent component",
+      showModal: false,
+    };
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1 {
+  background-color: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
+}
+</style>
+-->
+
+<!-- EMITTING CUSTOM EVENTS
+<template>
+  <h1>{{ title }}</h1>
+  <p>Welcome ...</p>
+  <div v-if="showModal">
+    <Modal :header="header" :text="text" theme="sale" @close="toggleModal" />
+  </div>
+  <button @click="toggleModal">Show Modal</button>
+</template>
+
+<script>
+import Modal from "./components/Modal.vue";
+
+export default {
+  name: "App",
+  components: { Modal },
+  data() {
+    return {
+      title: "My first vue App :) ",
+      header: "Please sign up here",
+      text: "It is all coming from App.vue which is the parent component",
+      showModal: false,
+    };
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1 {
+  background-color: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
+}
+</style>
+ -->
+
+<!-- PROPS 
+<template>
+  <p>Hi gee</p>
+  <h1>{{ title }}</h1>
+  <Modal :header="header" :text="text" theme="sale" />
+</template>
+
+<script>
+import Modal from "./components/Modal.vue";
+
+export default {
+  name: "App",
+  components: { Modal },
+  data() {
+    return {
+      title: "My first vue App :) ",
+      header: "Please sign up here",
+      text: "It is all coming from App.vue which is the parent component",
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1 {
+  background-color: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
+}
+</style>
+-->
+
+<!-- MULTIPLE COMPONENTS 
+<template>
+  <p>Hi gee</p>
+  <h1>{{ title }}</h1>
+  <Modal />
+</template>
+
+<script>
+import Modal from "./components/Modal.vue";
+
+export default {
+  name: "App",
+  components: { Modal },
+  data() {
+    return { title: "My first vue App :) " };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1 {
+  background-color: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
+}
+</style>
+-->
+
+<!-- TEMPLATE REFS -->
+<!-- 
+<template>
+  <h1>{{ title }}</h1>
+  <input type="text" ref="name" />
+  <button @click="handleClick">Click Me</button>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return { title: "My first vue App :) " };
+  },
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name);
+      this.$refs.name.classList.add("active");
+      this.$refs.name.focus();
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1 {
+  background-color: black;
+}
+</style>
+ -->
---
+++ src/components/Modal.vue
@@ -0,0 +1,380 @@
+<!--USING TELEPORT - CHANGES IN INDEX.HTML-->
+<template>
+  <div class="backdrop" @click.self="closeModal">
+    <div class="modal" :class="{ sale: theme === 'sale' }">
+      <slot>Defaul Slot</slot>
+      <div class="actions">
+        <slot name="links-one"></slot>
+      </div>
+      <div class="actions">
+        <slot name="links-two"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["theme"],
+  methods: {
+    closeModal() {
+      this.$emit("close");
+    },
+  },
+};
+</script>
+
+<style>
+.modal {
+  font-family: Arial, Helvetica, sans-serif;
+  width: 400px;
+  padding: 20px;
+  margin: 100px auto;
+  background: white;
+  border-radius: 10px;
+}
+.sale {
+  font-family: Arial, Helvetica, sans-serif;
+  width: 400px;
+  padding: 20px;
+  margin: 100px auto;
+  background: green;
+  border-radius: 10px;
+}
+.backdrop {
+  top: 0;
+  position: fixed;
+  background: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100%;
+}
+.modal h1 {
+  color: tomato;
+}
+.modal p {
+  font-style: normal;
+}
+.modal .actions {
+  text-align: center;
+  margin: 30px 0px 10px 0px;
+  color: #333;
+}
+</style>
+
+<!--CHALLENGE - CREATING ANOTHER MODAL
+<template>
+  <div class="backdrop" @click.self="closeModal">
+    <div class="modal" :class="{ sale: theme === 'sale' }">
+      <slot>Defaul Slot</slot>
+      <div class="actions">
+        <slot name="links-one"></slot>
+      </div>
+      <div class="actions">
+        <slot name="links-two"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["theme"],
+  methods: {
+    closeModal() {
+      this.$emit("close");
+    },
+  },
+};
+</script>
+
+<style>
+.modal {
+  font-family: Arial, Helvetica, sans-serif;
+  width: 400px;
+  padding: 20px;
+  margin: 100px auto;
+  background: white;
+  border-radius: 10px;
+}
+.sale {
+  font-family: Arial, Helvetica, sans-serif;
+  width: 400px;
+  padding: 20px;
+  margin: 100px auto;
+  background: green;
+  border-radius: 10px;
+}
+.backdrop {
+  top: 0;
+  position: fixed;
+  background: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100%;
+}
+.modal h1 {
+  color: tomato;
+}
+.modal p {
+  font-style: normal;
+}
+.modal .actions {
+  text-align: center;
+  margin: 30px 0px 10px 0px;
+  color: #333;
+}
+</style>
+-->
+
+<!--SLOTS - FOR SENDING CUSTOM TEMPLATES TO OTHER COMPONENTS
+<template>
+  <div class="backdrop" @click.self="closeModal">
+    <div class="modal" :class="{ sale: theme === 'sale' }">
+      <slot>Defaul Slot</slot>
+      <div class="actions">
+        <slot name="links"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["theme"],
+  methods: {
+    closeModal() {
+      this.$emit("close");
+    },
+  },
+};
+</script>
+
+<style>
+.modal {
+  font-family: Arial, Helvetica, sans-serif;
+  width: 400px;
+  padding: 20px;
+  margin: 100px auto;
+  background: white;
+  border-radius: 10px;
+}
+.sale {
+  font-family: Arial, Helvetica, sans-serif;
+  width: 400px;
+  padding: 20px;
+  margin: 100px auto;
+  background: green;
+  border-radius: 10px;
+}
+.backdrop {
+  top: 0;
+  position: fixed;
+  background: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100%;
+}
+.modal h1 {
+  color: tomato;
+}
+.modal p {
+  font-style: normal;
+}
+.modal .actions {
+  text-align: center;
+  margin: 30px 0px 10px 0px;
+  color: #333;
+}
+</style>
+-->
+
+<!-- CLICK EVENTS MODIFIERS
+<template>
+  <div class="backdrop" @click.self="closeModal">
+    <div class="modal" :class="{ sale: theme === 'sale' }">
+      <h1>{{ header }}</h1>
+      <p>{{ text }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["header", "text", "theme"],
+  methods: {
+    closeModal() {
+      this.$emit("close");
+    },
+  },
+};
+</script>
+
+<style>
+.modal {
+  font-family: Arial, Helvetica, sans-serif;
+  width: 400px;
+  padding: 20px;
+  margin: 100px auto;
+  background: white;
+  border-radius: 10px;
+}
+.sale {
+  font-family: Arial, Helvetica, sans-serif;
+  width: 400px;
+  padding: 20px;
+  margin: 100px auto;
+  background: green;
+  border-radius: 10px;
+}
+.backdrop {
+  top: 0;
+  position: fixed;
+  background: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100%;
+}
+.modal h1 {
+  color: tomato;
+}
+.modal p {
+  font-style: normal;
+}
+</style>
+-->
+
+<!-- EMITTING CUSTOM EVENTS
+<template>
+  <div class="backdrop" @click="closeModal">
+    <div class="modal" :class="{ sale: theme === 'sale' }">
+      <h1>{{ header }}</h1>
+      <p>{{ text }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["header", "text", "theme"],
+  methods: {
+    closeModal() {
+      this.$emit("close");
+    },
+  },
+};
+</script>
+
+<style>
+.modal {
+  font-family: Arial, Helvetica, sans-serif;
+  width: 400px;
+  padding: 20px;
+  margin: 100px auto;
+  background: white;
+  border-radius: 10px;
+}
+.sale {
+  font-family: Arial, Helvetica, sans-serif;
+  width: 400px;
+  padding: 20px;
+  margin: 100px auto;
+  background: green;
+  border-radius: 10px;
+}
+.backdrop {
+  top: 0;
+  position: fixed;
+  background: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100%;
+}
+.modal h1 {
+  color: tomato;
+}
+.modal p {
+  font-style: normal;
+}
+</style>
+-->
+
+<!-- PROPS 
+<template>
+  <div class="backdrop">
+    <div class="modal" :class="{ sale: theme === 'sale' }">
+      <h1>{{ header }}</h1>
+      <p>{{ text }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["header", "text", "theme"],
+};
+</script>
+
+<style>
+.modal {
+  font-family: Arial, Helvetica, sans-serif;
+  width: 400px;
+  padding: 20px;
+  margin: 100px auto;
+  background: white;
+  border-radius: 10px;
+}
+.sale {
+  font-family: Arial, Helvetica, sans-serif;
+  width: 400px;
+  padding: 20px;
+  margin: 100px auto;
+  background: green;
+  border-radius: 10px;
+}
+.backdrop {
+  top: 0;
+  position: fixed;
+  background: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100%;
+}
+.modal h1 {
+  color: tomato;
+}
+.modal p {
+  font-style: normal;
+}
+</style>
+-->
+
+<!-- MULTIPLE COMPONENETS AND SCOPED STYLIN 
+<template>
+  <div class="backdrop">
+    <div class="modal">
+      <h1>Modal Title</h1>
+      <p>Modal content</p>
+    </div>
+  </div>
+</template>
+
+<style>
+.modal {
+  font-family: Arial, Helvetica, sans-serif;
+  width: 400px;
+  padding: 20px;
+  margin: 100px auto;
+  background: white;
+  border-radius: 10px;
+}
+.backdrop {
+  top: 0;
+  position: fixed;
+  background: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100%;
+}
+/* .modal is class given to h1 above */
+.modal h1 {
+  color: tomato;
+}
+/* .modal p {
+  font-style: normal;
+} */
+</style>
+-->
